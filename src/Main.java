97a442f083ee49fa726d2153880a96d62d6db7fb
Jorge <jorge.martinezcruz@gmail.com>
2025-06-07 13:41:24 -0300

    main para ver mejor los resultados.

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,25 @@ public class Main {
     public static void main(String[] args) {
         try {
             SistemaDeCrafteo sistemaDeCrafteo= new SistemaDeCrafteo();
-
+            System.out.println("=====RECETARIO INICIAL========");
+            System.out.println(sistemaDeCrafteo.getRecetario());
+            System.out.println("=====INVENTARIO INICIAL=======");
+            System.out.println(sistemaDeCrafteo.getInventario());
+            System.out.println("=====INGREDIENTES BASICOS=====");
             for (Receta receta : sistemaDeCrafteo.getRecetario().getRecetas() ) {
                 System.out.println("Prueba Ingredientes básicos: " + receta.getObjetoProducido().getNombre());
                 System.out.println(sistemaDeCrafteo.ingredientesBasicos(receta.getObjetoProducido()));
             }
-            System.out.println("==============================");
+            System.out.println("=======PRUEBA CRAFTEO=========");
             for (Receta receta : sistemaDeCrafteo.getRecetario().getRecetas() ) {
                 System.out.println("Prueba craftear: " + receta.getObjetoProducido().getNombre());
                 sistemaDeCrafteo.craftear(receta);
                 System.out.println();
             }
-
+            System.out.println("=====INVENTARIO FINAL=========");
             System.out.println(sistemaDeCrafteo.getInventario());
-            System.out.println(sistemaDeCrafteo.getRecetario());
-
+            
+            System.out.println("=====GUARDAR XML==============");
             sistemaDeCrafteo.getInventario().guardarInventario();
             sistemaDeCrafteo.getRecetario().guardarRecetario();
         }catch(Exception e){
